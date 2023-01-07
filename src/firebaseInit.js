58182ddd322a022780956cfd83b233a62a2654df
PRANAV K D <pkdart13@gmail.com>
2023-01-07 12:35:10 +0530

    Update firebaseInit.js

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app"; 
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_APIKEY,
-  authDomain: process.env.VUE_APP_AUTHDOMAIN,
-  projectId: process.env.VUE_APP_PROJECTID,
-  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
-  appId: process.env.VUE_APP_APPID
+  apiKey: "AIzaSyBh9LH6nKP_PewPbG-x71nM3mNJQXFWzs8",
+  authDomain: "mrtyres-deeg.firebaseapp.com",
+  projectId: "mrtyres-deeg",
+  storageBucket: "mrtyres-deeg.appspot.com",
+  messagingSenderId: "338602839184",
+  appId: "1:338602839184:web:762ed110efe9529c7c5c15"
 };
 
 const app = initializeApp(firebaseConfig);
